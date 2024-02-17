e7a3abf38ddd1610e90bf709d2eb865193500a53
daph <daphneyapjy@hotmail.com>
2024-02-17 19:08:59 +0000

    moved func to app.py

--- app.py
+++ app.py
@@ -3,39 +3,9 @@ from flask import Flask
 import sqlite3
 import os
 from flask import Flask, render_template, request, url_for, redirect
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.sql import func
-# from sqlalchemy import Column, ForeignKey
-
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] =\
-#         'sqlite:///' + os.path.join(basedir, 'database.db')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     __tablename__ = "user"
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(100), nullable=False)
-#     password = db.Column(db.String(30), nullable=False)
-#     def __repr__(self):
-#         return f'<User {self.username}>'
-    
-# class Transactions(db.Model):
-#     __tablename__ = "transactions"
-#     transaction_id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.ForeignKey("user.id")
-#     transaction_name = db.Column(db.String(50), nullable=False)
-#     transaction_date = db.Column(db.Date, unique=True, nullable=False)
-#     transaction_category = db.Column(db.Integer)
-#     necessity_index = db.Column(db.Integer)
-
-#     def __repr__(self):
-#         return f'<Transaction: {self.transaction_name}>'
 
 @app.route("/")
 def index():
@@ -52,3 +22,19 @@ def index():
 if __name__ == "__main__":
     app.run()
 
+
+@app.route("/add", methods=['POST'])
+def add_transaction(name, amount, date, necessity_index, category):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    # data = request
+
+    # with conn:
+        
+    #     cur.execute("INSERT INTO transactions (name, VALUES ()")
+
+def edit_transation():
+    return 0
+
+def delete_transaction():
+    return 0
