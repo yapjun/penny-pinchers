30b76d1d1ff2480184feffe440f8bdc40de1ad9f
daph <daphneyapjy@hotmail.com>
2024-02-17 14:34:59 +0000

    tweaked homepage

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
