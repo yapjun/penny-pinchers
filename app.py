54a384715598c04b81deca3ee0e6ce2e7110cc88
des <destinesie@gmail.com>
2024-02-17 16:25:23 +0000

    fixed errors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import time
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run()
